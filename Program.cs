14cfae9a581cba79384d754b3ac8abb02e91d119
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-06-19 11:55:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Maksim";
+            string surName = "Makarevic";
+            Console.WriteLine(name + ' ' + surName);
+            string name1;
+            name1 = name;
+            name = surName;
+            surName = name1;
+            Console.WriteLine(name + ' ' + surName);           
+        }
+    }
+}
